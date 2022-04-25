e11a0b7a1493b1f07d6304d68e9750eb309285b1
scintilla4evr <kacper17@outlook.com>
2022-04-25 18:45:17 +0200

    Added extra "songs" + README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Flerovium
+
+A simple audio player built with React & Redux.
+
+![Flerovium screenshot](screen.png)
+
+## Features
+
+* Audio playback control (play, pause, ...)
+* Track queue
+* A simple track library with separate Tracks, Artists and Albums tabs
+
+## Space for Improvement
+
+This is just a quick little project I've made to learn the basics of React Redux. If it were to become a more fully-featured audio playback app, it would need at least:
+
+* A more sophisticated library (searching, filtering by genre, custom playlists)
+* ID3 tag extraction from audio files for auto-filling track info (artist, album, cover art)
+* An improved queue with repeat and shuffle controls
+* Visualizations in sync with audio (MilkDrop, anyone?)
--- src/features/library/mock.ts
+++ src/features/library/mock.ts
@@ -19,11 +19,55 @@ export const mockLibrary: LibraryType = {
             sourceUrl: "audio/noise/noise2.mp3",
 
             trackNo: null
-        }
+        },
+
+        "protogon": {
+            title: "PROTOGON",
+            artistId: "sci",
+            albumId: "protogon",
+
+            sourceUrl: "audio/protogon.mp3",
+
+            trackNo: null
+        },
+        "exodus": {
+            title: "EXODUS",
+            artistId: "sci",
+            albumId: "exodus",
+
+            sourceUrl: "audio/exodus.mp3",
+
+            trackNo: null
+        },
+        "xineotron": {
+            title: "XI-NEOTRON",
+            artistId: "sci",
+            albumId: "xineotron",
+
+            sourceUrl: "audio/xineotron.mp3",
+
+            trackNo: null
+        },
+
+        "silence": {
+            title: "shhh...",
+            artistId: "shh",
+            albumId: "silence",
+
+            sourceUrl: "audio/silence.mp3",
+
+            trackNo: null
+        },
     },
     artists: {
         "osc1llator": {
             name: "OSC1LLATOR"
+        },
+        "sci": {
+            name: "sci"
+        },
+        "shh": {
+            name: "Shhh....."
         }
     },
     albums: {
@@ -37,6 +81,51 @@ export const mockLibrary: LibraryType = {
             tracks: [
                 "noise1",
                 "noise2"
+            ]
+        },
+        "protogon": {
+            title: "PROTOGON",
+            
+            artistId: "sci",
+
+            coverUrl: "cover/protogon.png",
+
+            tracks: [
+                "protogon"
+            ]
+        },
+        "exodus": {
+            title: "EXODUS",
+            
+            artistId: "sci",
+
+            coverUrl: "cover/exodus.png",
+
+            tracks: [
+                "exodus"
+            ]
+        },
+        "xineotron": {
+            title: "XI-NEOTRON",
+            
+            artistId: "sci",
+
+            coverUrl: "cover/xineotron.png",
+
+            tracks: [
+                "xineotron"
+            ]
+        },
+
+        "silence": {
+            title: "SHHHH....",
+            
+            artistId: "shh",
+
+            coverUrl: "cover/silence.png",
+
+            tracks: [
+                "silence"
             ]
         }
     }
